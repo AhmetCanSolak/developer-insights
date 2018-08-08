01184e1516bbce73ece9c033558bc08191855410
AhmetCanSolak <asolak14@ku.edu.tr>
2018-08-08 11:52:34 +0300

    dashboard template update

--- dashboard/app.js
+++ dashboard/app.js
@@ -19,29 +19,7 @@ const Responses = {
 
 Vue.component('responses', Responses);
 
-const NotFound = { template: '<p>Page not found</p>' }
-const Home = { template: '<p>home page</p>' }
-const About = { template: '<p>about page</p>' }
-
-const routes = {
-  '/'      : Responses,
-  '/first' : Responses,
-  '/dashboard/' : Responses,
-  '/second': Responses,
-  '/third' : Responses,
-  '/about' : About
-}
 
 new Vue({
-  el: '#app',
-  data: {
-      currentRoute: window.location.pathname
-  },
-  computed: {
-      ViewComponent () {
-        console.log(this.currentRoute);
-        return routes[this.currentRoute] || NotFound
-      }
-  },
-  render (h) { return h(this.ViewComponent) }
+  el: '#app'
 });
